e78948ef3b1a8f92e87aca83961ff600e92901bc
EC2 Default User <abcdquont1234@gmail.com>
2021-02-19 14:49:16 +0000

    post_comment作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
 end
