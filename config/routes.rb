30266912b4ad65aaa9348edb6e948a40d48746ca
EC2 Default User <abcdquont1234@gmail.com>
2021-02-23 14:42:45 +0000

    my page追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_for :users
   get 'top' => 'homes#top'
   get 'top' => 'homes#about'
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 
 end
